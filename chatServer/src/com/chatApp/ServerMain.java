b49240903d8311119982693504cb00f4315fcfed
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-07-29 20:55:38 +0530

    added a controller

--- chatServer/src/com/chatApp/ServerMain.java
+++ chatServer/src/com/chatApp/ServerMain.java
@@ -1,11 +1,16 @@
 package com.chatApp;
 
+import com.controller.Controller;
+
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class ServerMain {
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
         int port = 8800;
-        Server server = new Server(port);
+        Controller controller = new Controller();
+        Server server = new Server(port, controller);
         server.start();
+
     }
 }
