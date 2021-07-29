ff2675a68a2be2a0449a1c1d71ff4753357e1c0e
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-07-29 21:58:48 +0530

    implemented registration command

--- chatServer/src/com/chatApp/ServerWorker.java
+++ chatServer/src/com/chatApp/ServerWorker.java
@@ -4,6 +4,7 @@ import com.controller.Controller;
 
 import java.io.*;
 import java.net.Socket;
+import java.sql.SQLException;
 
 public class ServerWorker extends Thread{
     Socket clientSocket = null;
@@ -23,20 +24,39 @@ public class ServerWorker extends Thread{
     public void run(){
         try {
             handleClientSocket();
-        } catch (IOException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private void handleClientSocket() throws IOException {
+    private void handleClientSocket() throws IOException, SQLException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String input;
         while((input = reader.readLine()) != null){
-            String[] tokens = input.split(" ", 3);
+            String[] tokens = input.split(" ");
             String cmd = tokens[0];
             if("login".equalsIgnoreCase(cmd)){
                 handleLogIn(tokens);
             }
+            if("register".equalsIgnoreCase(cmd)){
+                handleRegister(tokens);
+            }
+        }
+    }
+
+    //command format : register <username> <password>
+    private void handleRegister(String[] tokens) throws SQLException, IOException {
+        if(tokens.length == 3){
+            String username = tokens[1];
+            String password = tokens[2];
+            boolean isSuccessfulRegistration = controller.addUser(username, password);
+            if(isSuccessfulRegistration){
+                outputStream.write("registration successful\n".getBytes());
+            }else{
+                outputStream.write("username already exists\n".getBytes());
+            }
+        }else{
+            outputStream.write("unknown command\n".getBytes());
         }
     }
 
@@ -47,10 +67,12 @@ public class ServerWorker extends Thread{
             String password = tokens[2];
             boolean isValidLogin = controller.isValidUser(username, password);
             if(isValidLogin){
-                outputStream.write("login successful".getBytes());
+                outputStream.write("login successful\n".getBytes());
             }else{
-                outputStream.write("error login".getBytes());
+                outputStream.write("error login\n".getBytes());
             }
+        }else{
+            outputStream.write("unknown command\n".getBytes());
         }
     }
 }
