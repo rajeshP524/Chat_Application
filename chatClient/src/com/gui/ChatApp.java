cd8f68a99d5da37119174553eb1d2896aef996a0
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-08-05 20:26:18 +0530

    passed controller object

--- chatClient/src/com/gui/ChatApp.java
+++ chatClient/src/com/gui/ChatApp.java
@@ -11,10 +11,11 @@ public class ChatApp {
     public static void main(String[] args) throws UnknownHostException {
         Client client = new Client(InetAddress.getLocalHost(), 8800);
         client.connect();
+        Controller controller = new Controller(client);
         SwingUtilities.invokeLater(new Runnable(){
 
             public void run() {
-                new Authorization(client);
+                new Authorization(client, controller);
             }
         });
 
