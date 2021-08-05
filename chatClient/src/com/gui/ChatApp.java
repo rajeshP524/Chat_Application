18beeb6b2ef8000a245e4e7baecda06c06fa5f84
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-08-05 16:05:49 +0530

    application starter

---
+++ chatClient/src/com/gui/ChatApp.java
@@ -0,0 +1,23 @@
+package com.gui;
+
+import com.chatApp.Client;
+
+import javax.swing.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ChatApp {
+
+    public static void main(String[] args) throws UnknownHostException {
+        Client client = new Client(InetAddress.getLocalHost(), 8800);
+        client.connect();
+        SwingUtilities.invokeLater(new Runnable(){
+
+            public void run() {
+                new Authorization(client);
+            }
+        });
+
+
+    }
+}
