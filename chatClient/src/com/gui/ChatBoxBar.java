101e89bcaaa2cfeba6aad325855f3a30106de2d6
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-08-06 20:08:06 +0530

    register an event for send button

--- chatClient/src/com/gui/ChatBoxBar.java
+++ chatClient/src/com/gui/ChatBoxBar.java
@@ -4,12 +4,15 @@ import com.chatApp.Client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatBoxBar extends JPanel {
 
     private Client client;
     private JTextField chatField;
     private JButton send;
+    private ChatFieldListener chatFieldListener;
 
     public ChatBoxBar(Client client){
         this.client = client;
@@ -22,5 +25,32 @@ public class ChatBoxBar extends JPanel {
 
         add(new JScrollPane(chatField));
         add(send);
+
+        // adding listener to send button
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String msg = chatField.getText() + "\n";
+                chatField.setText("");
+                if(msg.length() == 1){
+                    //length == 1 indicates that there is only "\n"
+                }else{
+                    int count = 0;
+                    for(char c : msg.toCharArray()){
+                        if(c == ' ') count++;
+                    }
+
+                    if(count == msg.length() - 1) return;
+
+                    if(chatFieldListener != null){
+                        chatFieldListener.onChatMessage(msg);
+                    }
+                }
+            }
+        });
+    }
+
+    public void setChatFieldListener(ChatFieldListener chatFieldListener) {
+        this.chatFieldListener = chatFieldListener;
     }
 }
