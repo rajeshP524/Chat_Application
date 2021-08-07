451cec955f63ba0e7266a186baed1d87c0e5b739
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-08-07 15:51:23 +0530

    added functionalities for chatroom button

--- chatClient/src/com/gui/ToolBar.java
+++ chatClient/src/com/gui/ToolBar.java
@@ -17,6 +17,7 @@ public class ToolBar extends JPanel {
     private JButton exit;
 
     private DMessageListener dMessageListener;
+    private ChatroomLabelSetter chatroomLabelSetter;
 
     public ToolBar(Client client){
         this.client = client;
@@ -52,11 +53,13 @@ public class ToolBar extends JPanel {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String username = JOptionPane.showInputDialog(ToolBar.this, "Enter username:" );
-                if(username.length() == 0){
-                    JOptionPane.showMessageDialog(ToolBar.this, "No such user exits", "message", JOptionPane.ERROR_MESSAGE);
-                }
-                else{
-                    client.isValidUser(username);
+                if(username != null){
+                    if(username.length() == 0){
+                        JOptionPane.showMessageDialog(ToolBar.this, "No such user exits", "message", JOptionPane.ERROR_MESSAGE);
+                    }
+                    else{
+                        client.isValidUser(username);
+                    }
                 }
             }
         });
@@ -65,7 +68,48 @@ public class ToolBar extends JPanel {
         chatRoom.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                String username = JOptionPane.showInputDialog(ToolBar.this, "Enter a command: " );
+                String cmd = JOptionPane.showInputDialog(ToolBar.this, "Enter a command: join, leave, msg" );
+                if(cmd.equalsIgnoreCase("join")){
+                    cmd = "join chatroom\n";
+                    if(client.isMemberOfChatroom()){
+                        JOptionPane.showMessageDialog(ToolBar.this, "You are already a member of chatroom", "message", JOptionPane.ERROR_MESSAGE);
+                    }else{
+
+                        // add client to chatroom and upadate information in client
+                        client.joinChatroom(cmd);
+                        client.setMemberOfChatroom(true);
+                        JOptionPane.showMessageDialog(ToolBar.this, "You are added to chatroom", "message", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
+                else if(cmd.equalsIgnoreCase("leave")){
+                    cmd = "leave chatroom\n";
+
+                    if(client.isMemberOfChatroom()){
+                        // if you are a member of chatroom, you can exit and update information in client class
+                        client.leaveChatroom(cmd);
+                        client.setMemberOfChatroom(false);
+                        JOptionPane.showMessageDialog(ToolBar.this, "You are no more a member of chatroom", "message", JOptionPane.INFORMATION_MESSAGE);
+
+                    }else{
+                        JOptionPane.showMessageDialog(ToolBar.this, "You are already not a member of chatroom", "message", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+                else if(cmd.equalsIgnoreCase("msg")){
+                    if(client.isMemberOfChatroom()){
+                        // send messages at this room
+                        //set label to #chatroom in chatToolBar
+                        if(chatroomLabelSetter != null){
+                            chatroomLabelSetter.setLabel();
+                        }
+
+                    }else{
+                        // not allowed to send messages at this room
+                        JOptionPane.showMessageDialog(ToolBar.this, "You can't send messages at this room! execute JOIN command", "message", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+                else{
+                    JOptionPane.showMessageDialog(ToolBar.this, "unknown command", "error command", JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
 
@@ -87,5 +131,9 @@ public class ToolBar extends JPanel {
 
     public void setdMessageListener(DMessageListener dMessageListener) {
         this.dMessageListener = dMessageListener;
+    }
+
+    public void setChatroomLabelSetter(ChatroomLabelSetter chatroomLabelSetter) {
+        this.chatroomLabelSetter = chatroomLabelSetter;
     }
 }
