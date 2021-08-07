a7282b2b007e9882093296c1c97aef294bb74275
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-08-07 22:44:21 +0530

    Added user Icon for Identification of client(frontend)

--- chatClient/src/com/gui/ToolBar.java
+++ chatClient/src/com/gui/ToolBar.java
@@ -15,6 +15,8 @@ public class ToolBar extends JPanel {
     private JButton directMessage;
     private JButton chatRoom;
     private JButton exit;
+    private JLabel userIconLabel;
+    private JLabel userNameLabel;
 
     private DMessageListener dMessageListener;
     private ChatroomLabelSetter chatroomLabelSetter;
@@ -26,10 +28,23 @@ public class ToolBar extends JPanel {
         chatRoom = new JButton("ChatRoom");
         exit = new JButton("Exit");
 
+        //set icon
+        ImageIcon img = new ImageIcon("/home/rockzzz/Downloads/index.png");
+        Image i = img.getImage();
+        Image newImg = i.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+        img = new ImageIcon(newImg);
+        userIconLabel = new JLabel("", img, JLabel.CENTER);
+        String text = client.getUser();
+        userNameLabel = new JLabel("");
+        userNameLabel.setFont(new Font("verdana", Font.BOLD, 15));
+
         setLayout(new FlowLayout());
-        add(directMessage, FlowLayout.LEFT);
-        add(chatRoom, FlowLayout.LEFT);
-        add(exit, FlowLayout.RIGHT);
+        add(directMessage);
+        add(chatRoom);
+        add(exit);
+        add(new JLabel("   "));
+        add(userIconLabel);
+        add(userNameLabel);
 
         // add listener for exit button
         exit.addActionListener(new ActionListener() {
@@ -69,6 +84,7 @@ public class ToolBar extends JPanel {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String cmd = JOptionPane.showInputDialog(ToolBar.this, "Enter a command: join, leave, msg" );
+                if(cmd == null) return;
                 if(cmd.equalsIgnoreCase("join")){
                     cmd = "join chatroom\n";
                     if(client.isMemberOfChatroom()){
@@ -135,5 +151,9 @@ public class ToolBar extends JPanel {
 
     public void setChatroomLabelSetter(ChatroomLabelSetter chatroomLabelSetter) {
         this.chatroomLabelSetter = chatroomLabelSetter;
+    }
+
+    public void setUserNameLabelText(String text){
+        userNameLabel.setText(text);
     }
 }
